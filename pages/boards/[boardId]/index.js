dbf48654d85ab605733dc4ec20b3b32cbe0a0c9e
유신영 <shinyeong.yu@ipageon.com>
2024-03-07 15:42:04 +0900

    분기배포

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -11,7 +11,7 @@ export default function BoardsPage(){
     )
 }
 
-export const getServerSideProps = () => {
-    // 만약 서버사이드 렌더링을 하는 페이지라면? => out폴더로 생성 불가
-    //                                        => 이런 경우, next.config.js에서 exportPathMap으로 현재페이지 제외시키기
-}
+// export const getServerSideProps = () => {
+//     // 만약 서버사이드 렌더링을 하는 페이지라면? => out폴더로 생성 불가
+//     //                                        => 이런 경우, next.config.js에서 exportPathMap으로 현재페이지 제외시키기
+// }
